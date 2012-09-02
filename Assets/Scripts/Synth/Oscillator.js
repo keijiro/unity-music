4b2627a9a58eee008d9fc991f7e73b1654b52309
keijiro <keijiro@gmail.com>
2012-09-02 22:58:15 +0900

    Changed to use fastsin function.

--- Assets/Scripts/Synth/Oscillator.js
+++ Assets/Scripts/Synth/Oscillator.js
@@ -8,7 +8,17 @@ class Oscillator {
 	private var cx = 0.0;
 	private var step = 0.0;
 
-	static private var kPI2 = 6.28318530718;
+	static private var kPi = 3.14159265359;
+	static private var kPi2 = 6.28318530718;
+	static private var k4dPi = 1.27323954474;
+	static private var k4dPiPi = 0.40528473456;
+
+	// http://devmaster.net/forums/topic/4648-fast-and-accurate-sinecosine/
+	static private function fast_sin(x : float) {
+		x -= kPi;
+		x = k4dPi * x - k4dPiPi * x * Mathf.Abs(x);
+        return -0.225 * (x * Mathf.Abs(x) - x) - x;
+	}
 	
 	function SetNote(note : int) {
 		var freq = 440.0 * Mathf.Pow(2.0, 1.0 * (note - 69) / 12.0);
@@ -20,6 +30,8 @@ class Oscillator {
 		cx += step;
 		mx -= Mathf.Floor(mx);
 		cx -= Mathf.Floor(cx);
-		return Mathf.Sin(kPI2 * (cx + modulation * Mathf.Sin(kPI2 * mx)));
+		var x = cx + modulation * fast_sin(kPi2 * mx);
+		x -= Mathf.Floor(x);
+		return fast_sin(kPi2 * x);
 	}
 }
